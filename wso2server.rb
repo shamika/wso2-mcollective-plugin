25508b5445ff44666134d414ced3250360ba1db7
shamika <amilaaina@gmail.com>
2014-10-19 11:21:56 -0400

    V1 of completed wso2 agent plugin

--- wso2server.rb
+++ wso2server.rb
@@ -1,14 +1,11 @@
+require 'socket'
+
  module MCollective
    module Agent
      class Wso2server< RPC::Agent
-       metadata :name        => "wso2server",
-                :description => "Agent for MCollective to manupulate wso2 instances",
-                :author      => "Shamika",
-                :license     => "Apache",
-                :version     => "1.0",
-                :url         => "http://www.wso2.com",
-                :timeout     => 60
-
+  
+       @server_location = ""
+ 
        # Basic echo server
        action "echo" do
          validate :msg, String
@@ -17,10 +14,51 @@
          reply[:time] = Time.now.to_s
        end
       
-      # stop any wso2 server
-      action "start" do
-      
-      
+     # start any wso2 server
+     action "start_all" do
+      isCarbonHomeValid?      
+      system "#{@server_location}/wso2*/bin/wso2server.sh start"      
      end
+
+     # stop any wso2 server
+     action "stop_all" do
+      isCarbonHomeValid?      
+      system "#{@server_location}/wso2*/bin/wso2server.sh stop"
+     end
+     
+     # start wso2 server based on the hostname
+     action "start" do
+       validate :cluster, String
+       isCarbonHomeValid?
+        hostname = Socket.gethostname
+        if hostname.include? request[:cluster]
+           system "#{@server_location}/wso2*/bin/wso2server.sh start"
+           reply[:msg] = "Done"
+        else
+           reply[:msg] = "Not Interested"
+        end 
+     end
+
+     # stop wso2 server based on the hotname 
+     action "stop" do
+      validate :cluster, String
+       isCarbonHomeValid?
+        hostname = Socket.gethostname
+        if hostname.include? request[:cluster]
+           system "#{@server_location}/wso2*/bin/wso2server.sh stop"
+           reply[:msg] = "Done"
+        else
+           reply[:msg] = "Not Interested"
+        end 
+     end
+       
+     def isCarbonHomeValid?
+        @server_location = config.pluginconf["wso2server.location"]
+        if(@server_location.nil? or !File.exist?(@server_location))
+          reply[:msg] = "Wso2 server instance location not set or invalid " 
+          raise "Wso2 server instance location not set or invalid "
+        end
+     end
+    end
    end
  end
